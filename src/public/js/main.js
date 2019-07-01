35e74812f2c345094667f8cb92aff019ceb3ab83
carlosbritomorales <carloseduardobritomorales@gmail.com>
2019-06-30 20:01:48 -0400

    mejorando ubicación y aspecto gráfico de los maps

--- src/public/js/main.js
+++ src/public/js/main.js
@@ -3,7 +3,35 @@ var services2=document.getElementsByName("test2");
 var services3=document.getElementsByName("test3");
 var services4=document.getElementsByName("test4");
 
-var map = L.map('map-template').setView([ -33.044561, -71.612513], 20);
+var map = L.map('map-template');
+
+var redIcon = L.icon({
+  iconUrl: 'https://i.ibb.co/7rHDnDj/m1.png',
+  //shadowUrl: 'leaf-shadow.png',
+
+  iconSize:     [38, 38], // size of the icon
+  //shadowSize:   [50, 64], // size of the shadow
+  iconAnchor:   [22, 38], // point of the icon which will correspond to marker's location
+  shadowAnchor: [4, 62],  // the same for the shadow
+  popupAnchor:  [-3, -20] // point from which the popup should open relative to the iconAnchor
+});
+
+var browserLat;
+var browserLong;  
+
+navigator.geolocation.getCurrentPosition(function(position) {
+    browserLat =  position.coords.latitude;
+    browserLong = position.coords.longitude;
+ 
+    marker_actual = L.marker([browserLat,browserLong], {icon: redIcon}).addTo(map);
+    marker_actual.bindPopup('<b>Hola </b><br>Tu estas aqui').openPopup();
+    map.setView([browserLat,browserLong], 18);  
+    
+    console.log(browserLat);
+    console.log(browserLong);
+}, function(err) {
+    console.error(err);
+});
 
 const tileURL = 'https://b.tile.openstreetmap.org/{z}/{x}/{y}.png' 
 const tileURL2 = 'https://maps.wikimedia.org/osm-intl/{z}/{x}/{y}.png';
@@ -15,15 +43,15 @@ const tile = L.tileLayer(tileURL2);
 const socket = io.connect();
 
 // Custom icon
-var redIcon = L.icon({
-  iconUrl: 'https://i.ibb.co/wwxj8Fd/red.png',
+var blueIcon = L.icon({
+  iconUrl: 'https://i.ibb.co/4jLM99p/m2.png',
   //shadowUrl: 'leaf-shadow.png',
 
   iconSize:     [38, 38], // size of the icon
   //shadowSize:   [50, 64], // size of the shadow
-  iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
+  iconAnchor:   [22, 38], // point of the icon which will correspond to marker's location
   shadowAnchor: [4, 62],  // the same for the shadow
-  popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
+  popupAnchor:  [-3, -20] // point from which the popup should open relative to the iconAnchor
 });
 
 // Marker
@@ -32,7 +60,7 @@ marker.bindPopup('Hello There!');
 map.addLayer(marker);
 
 for(var i=0; i<services.length; i++){
-  marker = L.marker([services3[i].title,services2[i].title]); // kiev, ukraine
+  marker = L.marker([services3[i].title,services2[i].title], {icon: blueIcon}); // kiev, ukraine
   marker.bindPopup(services[i].title+"\nde\n"+services4[i].title);
   map.addLayer(marker)
 }
@@ -42,8 +70,8 @@ map.locate({enableHighAccuracy: true})
 map.on('locationfound', (e) => {
   const coords = [e.latlng.lat, e.latlng.lng];
   const newMarker = L.marker(coords,{icon: redIcon}).addTo(map);
-  newMarker.bindPopup('Estás justo aquí!');
-  map.addLayer(newMarker);
+  //newMarker.bindPopup('Estás justo aquí!');
+  //map.addLayer(newMarker);
   socket.emit('userCoordinates', e.latlng);
 });
 
--- src/public/js/map.js
+++ src/public/js/map.js
@@ -2,7 +2,48 @@
 
 //window.alert(services[0].title);
     
-var map = L.map('map-template').setView([ -33.044561, -71.612513], 20);
+var map = L.map('map-template');
+
+var browserLat;
+var browserLong;  
+
+// Custom icon
+var redIcon = L.icon({
+  iconUrl: 'https://i.ibb.co/7rHDnDj/m1.png',
+  //shadowUrl: 'leaf-shadow.png',
+
+  iconSize:     [38, 38], // size of the icon
+  //shadowSize:   [50, 64], // size of the shadow
+  iconAnchor:   [22, 38], // point of the icon which will correspond to marker's location
+  shadowAnchor: [4, 62],  // the same for the shadow
+  popupAnchor:  [-3, -20] // point from which the popup should open relative to the iconAnchor
+});
+
+// Custom icon
+var blueIcon = L.icon({
+  iconUrl: 'https://i.ibb.co/4jLM99p/m2.png',
+  //shadowUrl: 'leaf-shadow.png',
+
+  iconSize:     [38, 38], // size of the icon
+  //shadowSize:   [50, 64], // size of the shadow
+  iconAnchor:   [22, 38], // point of the icon which will correspond to marker's location
+  shadowAnchor: [4, 62],  // the same for the shadow
+  popupAnchor:  [-3, -20] // point from which the popup should open relative to the iconAnchor
+});
+
+navigator.geolocation.getCurrentPosition(function(position) {
+    browserLat =  position.coords.latitude;
+    browserLong = position.coords.longitude;
+ 
+    marker_actual = L.marker([browserLat,browserLong], {icon: redIcon}).addTo(map);
+    marker_actual.bindPopup('<b>Hola </b><br>Tu estas aqui').openPopup();
+    map.setView([browserLat,browserLong], 18);  
+    
+    console.log(browserLat);
+    console.log(browserLong);
+}, function(err) {
+    console.error(err);
+});
 
 const tileURL = 'https://b.tile.openstreetmap.org/{z}/{x}/{y}.png' 
 const tileURL2 = 'https://maps.wikimedia.org/osm-intl/{z}/{x}/{y}.png';
@@ -20,7 +61,7 @@ map.addLayer(marker);
 
 map.on('click', function(e) { 
   map.removeLayer(marker);
-  marker = L.marker([e.latlng.lat, e.latlng.lng]); 
+  marker = L.marker([e.latlng.lat, e.latlng.lng], {icon: blueIcon}); 
   marker.bindPopup('¿Aquí trabajas?'); 
   map.addLayer(marker); 
   document.getElementById("longitude").value=e.latlng.lat; 
@@ -32,8 +73,8 @@ map.locate({enableHighAccuracy: true})
 map.on('locationfound', (e) => {
   const coords = [e.latlng.lat, e.latlng.lng];
   const newMarker = L.marker(coords);
-  newMarker.bindPopup('Estás justo aquí!');
-  map.addLayer(newMarker);
+  //newMarker.bindPopup('Estás justo aquí!');
+  //map.addLayer(newMarker);
   socket.emit('userCoordinates', e.latlng);
 });
 
